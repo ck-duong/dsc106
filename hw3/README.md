39657b49d60a148d1c3b69979686339ab0e9c629
Casey Duong <ckduong14@gmail.com>
2019-11-13 21:48:19 -0800

    final details

--- hw3/README.md
+++ hw3/README.md
@@ -11,14 +11,15 @@
 - Open preview in new browser for best view (button is on bottom left, next to the refresh button)
 
 ## Notes
-- To toggle between the pie chart and the table/bar chart of energy breakdown:
+- **To toggle between the pie chart and the table/bar chart of energy breakdown:**
     1. Click on the three bars in the upper right corner of the pie chart
     2. Click "Switch Chart"
+- **Generation Chart**: In the screencast, the Generation chart included the load values for exports and pumps, which are negative values since they do not generate energy but rather use it. In my recreation of the dashboard, I included this and plotted both the exports and pumps within the Generation chart. It is worth noting that the presence of exports and pumps goes against the chart title of "Generation" of energy since they both use up energy. However, plotting them is can be useful because you can compare both sides of the x-axis to see generated energy versus spent energy. In my dashboard, I kept the title as "Generation MW" despite this because the chart continues to show the Generation of energy on a timeseries, which is its primary goal and functionality, and just happens to also display the usage as well.
 
 ## Limitations
 In my recreation of the given Springfield Energy dashboard, I wasn't able to completely replicate all the features present in the screencast. Some of the issues I had (that are present in my final submission) were:
 - **Syncing the three charts on the SharedGrid**: When trying to sync my graphs, they always synchronized inconsistently, meaning that only two of the three graphs would synchronize or graphs would only synchronize with charts above them (ie. Generation doesn't synchronize with Temperature unless you're scrolling on Temperature)
-- **Issue with showing the correct value on the area graph**: When scrolling over the power area chart, the export value is always printed rather than the actual value you're hovering over (ie. hydro, wind, etc).
+- **Issue with showing the correct value on the area graph**: When scrolling over the power generation area chart, the export value is always printed rather than the actual value you're hovering over (ie. hydro, wind, etc). To fix this, I wanted to try looping through the different series present in the stacked area chart, but wasn't able to come up with a solution that both displayed the correct data and synchronised the chart with the other charts. However, you can see the true values in the legend when you scroll over points. Rather than displaying the export value when trying to scroll, I just omitted that feature and displayed the date and total.
 - **Updating Values**: For updating values (most notably the legend), at points I had to apply non-ideal methods to update the values such as directly calling the DOM and manually updating the values.
 - **Aesthetics**: I could not fully match the screencast aesthetics in my dashboard, but still aimed to create a visually pleasing dashboard that resembles the original. Though I match most of the basic functionality, aethetically they are lacking.
 
--- hw3/main.js
+++ hw3/main.js
@@ -3,10 +3,10 @@
  * built-in events with handlers defined on the parent element.
  */
 
-["mousemove", "touchmove", "touchstart"].forEach(function (eventType) {
+["mousemove", "touchmove", "touchstart"].forEach(function(eventType) {
   document
     .getElementById("sharedGrid")
-    .addEventListener(eventType, function (e) {
+    .addEventListener(eventType, function(e) {
       var chart, point, i, event;
 
       for (i = 0; i < 3; i = i + 1) {
@@ -35,7 +35,7 @@
  * Override the reset function, we don't need to hide the tooltips and
  * crosshairs.
  */
-Highcharts.Pointer.prototype.reset = function () {
+Highcharts.Pointer.prototype.reset = function() {
   Highcharts.charts[3].setTitle({
     text: "Avg<br></br><b>7020 MW</b>"
   });
@@ -74,7 +74,7 @@ var globalPoint;
 /**
  * Highlight a point by showing tooltip, setting hover state and draw crosshair
  */
-Highcharts.Point.prototype.highlight = function (event) {
+Highcharts.Point.prototype.highlight = function(event) {
   event = this.series.chart.pointer.normalize(event);
   this.onMouseOver(); // Show the hover marker
   this.series.chart.tooltip.refresh(this); // Show the tooltip
@@ -90,7 +90,7 @@ function syncExtremes(e) {
 
   if (e.trigger !== "syncExtremes") {
     // Prevent feedback loop
-    Highcharts.each(Highcharts.charts, function (chart) {
+    Highcharts.each(Highcharts.charts, function(chart) {
       if (chart !== thisChart) {
         if (chart.xAxis[0].setExtremes) {
           // It is null while updating
@@ -105,7 +105,8 @@ function syncExtremes(e) {
 
 let sharedConfig = {
   layout: "3x1",
-  graphset: [{
+  graphset: [
+    {
       // config for the energy stacked area graph
       chart: {
         type: "area",
@@ -182,23 +183,24 @@ let sharedConfig = {
       },
       tooltip: {
         shared: false,
-        positioner: function () {
+        positioner: function() {
           return {
             // right aligned
             x: this.chart.chartWidth - this.label.width,
             y: 10 // align to title
           };
         },
-        formatter: function () {
+        formatter: function() {
           return (
-            Highcharts.dateFormat("%e %b, %l:%M %p", new Date(this.x)) +
-            " | " +
-            this.series.name +
-            " " +
             "<b>" +
-            this.y +
-            " MW | " +
-            "</b> " +
+            Highcharts.dateFormat("%e %b, %l:%M %p", new Date(this.x)) +
+            "</b> | " +
+            //            this.series.name +
+            //            " " +
+            //            "<b>" +
+            //            this.y +
+            //            " MW | " +
+            //            "</b> " +
             "Total <b>" +
             this.total.toFixed(0) +
             " MW" +
@@ -219,9 +221,11 @@ let sharedConfig = {
       credits: {
         enabled: false
       },
-      series: [{
-        data: []
-      }]
+      series: [
+        {
+          data: []
+        }
+      ]
     },
     {
       // config for the price line graph
@@ -231,7 +235,7 @@ let sharedConfig = {
         height: 200
       },
       title: {
-        text: "Price $/MWh",
+        text: "<b>Price</b> $/MWh",
         fontSize: 18,
         align: "left"
       },
@@ -286,14 +290,14 @@ let sharedConfig = {
       },
       tooltip: {
         shared: true,
-        positioner: function () {
+        positioner: function() {
           return {
             // right aligned
             x: this.chart.chartWidth - this.label.width,
             y: 10 // align to title
           };
         },
-        formatter: function () {
+        formatter: function() {
           return (
             "<b>" +
             Highcharts.dateFormat("%e %b, %l:%M %p", new Date(this.x)) +
@@ -323,7 +327,7 @@ let sharedConfig = {
         height: 200
       },
       title: {
-        text: "Temperature °F",
+        text: "<b>Temperature</b> °F",
         fontSize: 18,
         align: "left"
       },
@@ -378,14 +382,14 @@ let sharedConfig = {
       },
       tooltip: {
         shared: true,
-        positioner: function () {
+        positioner: function() {
           return {
             // right aligned
             x: this.chart.chartWidth - this.label.width,
             y: 10 // align to title
           };
         },
-        formatter: function () {
+        formatter: function() {
           return (
             "<b>" +
             Highcharts.dateFormat("%e %b, %l:%M %p", new Date(this.x)) +
@@ -415,10 +419,11 @@ let pieConfig = {
     menuItemDefinitions: {
       // Custom definition
       switchChart: {
-        onclick: function () {
+        onclick: function() {
           var chartType = this.options.chart.type;
 
-          if (chartType === "bar") {}
+          if (chartType === "bar") {
+          }
           this.update({
             chart: {
               type: chartType === "bar" ? "pie" : "bar"
@@ -479,11 +484,13 @@ let pieConfig = {
       }
     }
   },
-  series: [{
-    name: "Energy",
-    colorByPoint: true,
-    data: []
-  }]
+  series: [
+    {
+      name: "Energy",
+      colorByPoint: true,
+      data: []
+    }
+  ]
 };
 
 // global data-structure to hold the energy breakup
@@ -505,7 +512,7 @@ function updateGlobalEnergyData(data) {
 }
 
 function updateLegend(nodeId) {
-  var dataset = globalEnergyData["keys"].map(function (elm, idx) {
+  var dataset = globalEnergyData["keys"].map(function(elm, idx) {
     return {
       name: elm,
       y: [globalEnergyData["values"][nodeId][idx]][0]
@@ -578,7 +585,7 @@ function updateLegend(nodeId) {
 // the nodeId is basically the x-axis value
 // the actual breakup is retrieved from the global data-structure
 function renderPieChart(nodeId) {
-  var pieDataSet = globalEnergyData["keys"].map(function (elm, idx) {
+  var pieDataSet = globalEnergyData["keys"].map(function(elm, idx) {
     if (elm !== "exports" && elm !== "pumps") {
       return {
         name: elm,
@@ -587,13 +594,15 @@ function renderPieChart(nodeId) {
     }
   });
   pieDataSet.push(pieDataSet.splice(4, 1)[0]);
-  pieConfig.series = [{
-    name: "Energy Generated",
-    colorByPoint: true,
-    data: pieDataSet,
-    innerSize: "50%",
-    size: "90%"
-  }];
+  pieConfig.series = [
+    {
+      name: "Energy Generated",
+      colorByPoint: true,
+      data: pieDataSet,
+      innerSize: "50%",
+      size: "90%"
+    }
+  ];
 
   var pieChart = document.createElement("div");
   pieChart.className = "chart";
@@ -603,7 +612,7 @@ function renderPieChart(nodeId) {
 
 function updatePieChart(nodeId) {
   var totalPie = 0;
-  var pieDataSet = globalEnergyData["keys"].map(function (elm, idx) {
+  var pieDataSet = globalEnergyData["keys"].map(function(elm, idx) {
     if (elm !== "exports" && elm !== "pumps") {
       totalPie += [globalEnergyData["values"][nodeId][idx]][0];
       return {
@@ -613,18 +622,21 @@ function updatePieChart(nodeId) {
     }
   });
   pieDataSet.push(pieDataSet.splice(4, 1)[0]);
-  pieConfig.series = [{
-    name: "Energy Generated (MW)",
-    colorByPoint: true,
-    data: pieDataSet,
-    innerSize: "40%",
-    size: "90%"
-  }];
+  pieConfig.series = [
+    {
+      name: "Energy Generated (MW)",
+      colorByPoint: true,
+      data: pieDataSet,
+      innerSize: "40%",
+      size: "90%"
+    }
+  ];
 
   document.getElementById("pieGrid");
   Highcharts.charts[3].series[0].setData(
     pieDataSet,
-    true, {
+    true,
+    {
       duration: 10
     },
     true
@@ -641,13 +653,13 @@ function updatePieChart(nodeId) {
 // It also plots the pie chart for nodeId=0
 function onSuccessCb(jsonData) {
   var energyData = jsonData
-    .filter(function (elm) {
+    .filter(function(elm) {
       return elm["type"] === "power";
     })
-    .map(function (elm) {
+    .map(function(elm) {
       if (elm.fuel_tech === "exports" || elm.fuel_tech === "pumps") {
         return {
-          data: elm["history"]["data"].map(function (x) {
+          data: elm["history"]["data"].map(function(x) {
             return x * -1;
           }),
           name: elm["id"].split(".")[elm["id"].split(".").length - 2],
@@ -664,10 +676,10 @@ function onSuccessCb(jsonData) {
     });
   updateGlobalEnergyData(energyData);
   var priceData = jsonData
-    .filter(function (elm) {
+    .filter(function(elm) {
       return elm["type"] === "price";
     })
-    .map(function (elm) {
+    .map(function(elm) {
       return {
         data: elm["history"]["data"],
         name: elm["id"].split(".")[1],
@@ -675,17 +687,17 @@ function onSuccessCb(jsonData) {
       };
     });
   var tempData = jsonData
-    .filter(function (elm) {
+    .filter(function(elm) {
       return elm["type"] === "temperature";
     })
-    .map(function (elm) {
+    .map(function(elm) {
       return {
         data: elm["history"]["data"],
         name: elm["id"].split(".")[1],
         showInLegend: false
       };
     });
-  var start = jsonData.map(function (elm) {
+  var start = jsonData.map(function(elm) {
     if (elm["history"]["start"]) {
       return elm["history"]["start"] * 1000;
     }
@@ -730,7 +742,7 @@ function onSuccessCb(jsonData) {
 function fetchJSONFile(filePath, callbackFunc) {
   console.debug("Fetching file:", filePath);
   var httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function () {
+  httpRequest.onreadystatechange = function() {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200 || httpRequest.status === 0) {
         console.info("Loaded file:", filePath);
